92bf546c6bb4785711d45ad7312dcfea7df2f9cb
3aemon <theerasak.shadow@gmail.com>
2026-09-16 09:08:48 +0700

    fix: discover current Visual Studio Roslyn compiler

--- windows/build.bat
+++ windows/build.bat
@@ -1,14 +1,21 @@
 @echo off
-rem Build WhisperApp for Windows - no SDK needed, uses the Roslyn compiler
-rem bundled with VS 2019 Build Tools (falls back to the .NET Framework compiler).
+rem Build WhisperApp for Windows - no SDK needed, uses Roslyn from the newest
+rem installed Visual Studio / Build Tools, then falls back to legacy locations.
 setlocal
 
-set "CSC=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\Roslyn\csc.exe"
-if not exist "%CSC%" set "CSC=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe"
-if not exist "%CSC%" set "CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
+set "CSC="
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if exist "%VSWHERE%" (
+  for /f "usebackq tokens=*" %%I in (`"%VSWHERE%" -latest -products * -requires Microsoft.Component.MSBuild -find MSBuild\Current\Bin\Roslyn\csc.exe`) do set "CSC=%%I"
+)
+
+if not defined CSC if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\Roslyn\csc.exe" set "CSC=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\Roslyn\csc.exe"
+if not defined CSC if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe" set "CSC=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe"
+if not defined CSC set "CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
 
 set "FW=C:\Windows\Microsoft.NET\Framework64\v4.0.30319"
 
+echo Using C# compiler: %CSC%
 "%CSC%" /nologo /noconfig /nostdlib+ /target:winexe /platform:anycpu /optimize+ /langversion:7.3 /codepage:65001 /out:"%~dp0WhisperApp.exe" /r:"%FW%\mscorlib.dll" /r:"%FW%\System.dll" /r:"%FW%\System.Core.dll" /r:"%FW%\System.Drawing.dll" /r:"%FW%\System.Windows.Forms.dll" /r:"%FW%\System.Net.Http.dll" /r:"%FW%\System.Web.Extensions.dll" "%~dp0src\*.cs"
 
 if errorlevel 1 (
